ba77e090de9adba807ceae7573f0d1d3228baae3
FedeHorus <110682482+FedeHorus@users.noreply.github.com>
2022-09-19 20:37:02 -0300

    /

---
+++ factorial-for.js
@@ -0,0 +1,11 @@
+//factorial-for.js
+
+let factorial= 1;
+
+for  (let i = 10; i > 0 ;i-- ){
+    factorial *= i;
+    
+    
+}
+
+console.log(factorial);
---
+++ factorial-while.js
@@ -0,0 +1,11 @@
+//factorial-while.js
+
+let factorial= 10;
+let numin = 1
+
+while(factorial >1){
+    numin *= factorial
+    factorial--
+    
+}
+console.log(numin)
