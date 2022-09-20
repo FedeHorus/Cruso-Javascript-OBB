f5066e903fd9153ccc7fac85bc1375461f345add
FedeHorus <110682482+FedeHorus@users.noreply.github.com>
2022-09-19 21:47:19 -0300

    Create textos.js

---
+++ Javascript/textos.js
@@ -0,0 +1,18 @@
+
+let nombre= 'Federico';
+let apellido = 'Acevedo';
+let estudiante = nombre.concat(" ", apellido);
+let estudianteMayus = estudiante.toUpperCase();
+let estudianteMinus = estudiante.toLowerCase();
+let numeroletras = estudiante.length;
+let primeraLetraNombre = nombre.charAt(0);
+let finalApellido = apellido.slice(apellido.length - 1);
+let eliminaespacios = estudiante.trim();
+let nombreenestudiante = estudiante.includes(nombre);
+console.log(nombreenestudiante);
+console.log(eliminaespacios)
+console.log(estudianteMayus);
+console.log(estudianteMayus);
+console.log(estudiante)
+console.log(finalApellido)
+
