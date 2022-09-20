8bc18f2978b89247161c0f1376cb2e2317f5d59b
FedeHorus <110682482+FedeHorus@users.noreply.github.com>
2022-09-19 22:27:44 -0300

    Create numeros.js

---
+++ Numeros/numeros.js
@@ -0,0 +1,20 @@
+
+//altura en cm
+let altura = 178;
+
+//altura en metros
+let alturaM = 1.78;
+
+//peso en kg
+
+let peso = 82.5;
+
+//altura rdondeado hacia arriba
+let alturaRedondeado= Math.ceil(alturaM);
+
+//peso en kg redondeado hacia abajo
+let pesoRedondeado = Math.floor(peso);
+
+let iguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
+
+
