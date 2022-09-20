21db47e7284fa8c2b2547b6eb7bfd206c9786b4d
FedeHorus <110682482+FedeHorus@users.noreply.github.com>
2022-09-20 14:04:07 -0300

    Create conjuntos.js

---
+++ 07- Sets y Objetos/conjuntos.js
@@ -0,0 +1,11 @@
+//conjuntos.js
+
+let miSet = new Set (['Federico', 'Marysol','Carlos','Miranda']);
+console.log(miSet);
+
+miSet.add('Federico');
+console.log(miSet);
+
+miSet.add('Javascript');
+console.log(miSet)
+
