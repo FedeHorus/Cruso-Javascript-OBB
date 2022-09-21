5be161e6057cb70898275f01c463d0a532b1c89a
FedeHorus <110682482+FedeHorus@users.noreply.github.com>
2022-09-21 19:35:48 -0300

    Add files via upload

---
+++ ejercicio15/index.js
@@ -0,0 +1,17 @@
+
+const nombre = 'Federico';
+const apellido ='Acevedo';
+
+let objeto = {
+     nombre,
+     apellido,
+
+}
+
+//sessionStorage.setItem("objeto", JSON.stringify(objeto));
+//localStorage.setItem("objeto", JSON.stringify(objeto));
+
+const ahora = new Date();
+
+
+//document.cookie = `objeto=${JSON.stringify(objeto)}; expires=${new Date(ahora.getTime() +2 *60000)}`
