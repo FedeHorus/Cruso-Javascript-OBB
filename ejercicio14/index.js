804ef3220756636682a1ddf6f77610d54badb0b4
FedeHorus <110682482+FedeHorus@users.noreply.github.com>
2022-09-21 17:34:15 -0300

    Create index.js

---
+++ ejercicio14/index.js
@@ -0,0 +1,11 @@
+
+let boton = document.querySelector("#btn");
+
+
+boton.addEventListener("click", ()=> {
+    alert("click en el boton")
+})
+
+$("button").click(function(){
+    console.log("Hola, estoy utiizando JQuery");
+})
